2f102585cee7f996df63c6ff728d50ddc0d23d70
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-11-02 11:36:46 -0700

    tasks

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -30,8 +30,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		size_t l = 0, r = 0;
 
-		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 		return ((l > r) ? l : r);
 	}
 	return (0);
